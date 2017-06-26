ac9356779797aeede763d06dd336c551138825db
Beret <c445352742@126.com>
2017-06-26 13:52:17 +0800

    check

--- global/global.go
+++ global/global.go
@@ -1,14 +1,23 @@
 package _global
 
 import ()
+//全局已登录用户表结构
 type User struct {
 	Id string
 	Ip string
 	Name string
 	Time string
+	Loc string
 }
+
+//全局场景用户表结构
+type Scene struct {
+	Id string
+	Name string
+	Appearance []string
+}
+
 var Tcplist []User
-var Sum int=0
 
 func s()  {
 	var us User
