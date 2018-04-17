262f9826c5c5a58e2419070b611303343ac5a734
Beret <c445352742@126.com>
2018-04-17 13:55:38 +0800

    init

--- spawn/pack.js
+++ spawn/pack.js
@@ -5,23 +5,24 @@ console.log(chalk.yellowBright('The packager will generate package in 4 minutes 
 
 
 let options = {
-  arch: 'ia32',
-  name: "ATM",
-  asar: {
-    unpackDir: 'dist'
-  },
-  // asar:false,
-  dir: path.join(__dirname, '../'),
-  icon: path.join(__dirname, '../static/logo.ico'),
-  ignore: /^\/(build|installer|config|\.[a-z]+|README|static|src)/,
-  out: path.join(__dirname, '../out'),
-  overwrite: true,
-  platform: 'win32'
+    arch: 'ia32',
+    name: "Valkyrie",
+    asar: {
+        unpackDir: 'dist'
+    },
+    asar: false,
+    electronVersion: '1.8.4',
+    dir: path.join(__dirname, '../'),
+    icon: path.join(__dirname, '../static/logo.ico'),
+    ignore: /^\/(build|vue|installer|config|\.[a-z]+|README|static|src)/,
+    out: path.join(__dirname, '../out'),
+    overwrite: true,
+    platform: 'win32'
 }
 e(options, function (err, appPaths) {
-  if (err) {
-    console.log(chalk.redBright(err + '\n'));
-  } else {
-    console.log(appPaths);
-  }
+    if (err) {
+        console.log(chalk.redBright(err + '\n'));
+    } else {
+        console.log(appPaths);
+    }
 });
--- vue/config/index.js
+++ vue/config/index.js
@@ -38,10 +38,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
@@ -51,7 +51,7 @@ module.exports = {
 
     productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
+    devtool: false,
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
