1927a4805a1353125bc25ab69fe66aafbe6e4c27
Beret <c445352742@126.com>
2018-05-08 00:25:52 +0800

    ini

--- spawn/dev.js
+++ spawn/dev.js
@@ -1,7 +1,7 @@
 const cp = require('child_process');
 let exec = cp.exec;
 
-let devSvr = exec('cd ' + __dirname + '\\..\\vue&npm run dev', function (error, stdout, stderr) {
+var devSvr = exec('cd ' + __dirname + '\\..\\vue&npm run dev', function (error, stdout, stderr) {
     if (error) {
         console.error('error: ' + error);
         return;
@@ -11,34 +11,37 @@ let devSvr = exec('cd ' + __dirname + '\\..\\vue&npm run dev', function (error, 
 devSvr.stdout.on('data', function (msg) {
     console.log(msg);
 })
+devSvr.stderr.on('data', function (msg) {
+    console.log(msg);
+})
 devSvr.on('exit', function () {
     console.log('exit devSvr');
 });
 
-let ele = exec('cd ' + __dirname + '\\..&electron . --dev', function (error, stdout, stderr) {
-    if (error) {
-        console.error('error: ' + error);
-        return;
-    }
-    console.log(stdout);
-});
-ele.stdout.on('data', function (msg) {
-    console.log(msg);
-})
-ele.on('exit', function () {
-    console.log('exit ele');
-});
+// let ele = exec('./node_modules/.bin/electron . --dev', function (error, stdout, stderr) {
+//     if (error) {
+//         console.error('error: ' + error);
+//         return;
+//     }
+//     console.log(stdout);
+// });
+// ele.stdout.on('data', function (msg) {
+//     console.log(msg);
+// })
+// ele.on('exit', function () {
+//     console.log('exit ele');
+// });
 
 process.on('exit', function () {
     console.log('已退出');
-    devSvr.exit();
-    ele.exit();
+    // devSvr.kill('SIGHUP');
+    // devSvr._events.exit();
 })
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
     if (chunk === 'exit\r\n') {
-
+        devSvr.exit();
         process.exit();
     }
     else {
