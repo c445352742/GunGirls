32105d03c6f045f2cf3c3639aa6251a6d20ee39e
longhua.chai <longhua.chai@zhaopin.com.cn>
2018-05-09 17:54:00 +0800

    ini

--- spawn/dev.js
+++ spawn/dev.js
@@ -1,50 +1,35 @@
 const cp = require('child_process');
 let exec = cp.exec;
 
-var devSvr = exec('cd ' + __dirname + '/../vue&&npm run devVue', function (error, stdout, stderr) {
+// 查询mac端口 lsof -i tcp:8992
+
+// var devSvr = exec('cd ' + __dirname + '/../vue&&npm run devVue', function (error, stdout, stderr) {
+//   if (error) {
+//     console.error('error: ' + error);
+//     return;
+//   }
+//   console.log(stdout);
+// });
+// devSvr.stdout.on('data', function (msg) {
+//   console.log(msg);
+// })
+// devSvr.stderr.on('data', function (msg) {
+//   // console.log(msg);
+// })
+// devSvr.on('exit', function () {
+//   console.log('exit devSvr');
+// });
+
+let ele = exec('./node_modules/.bin/electron . --dev', function (error, stdout, stderr) {
     if (error) {
         console.error('error: ' + error);
         return;
     }
     console.log(stdout);
 });
-devSvr.stdout.on('data', function (msg) {
+ele.stdout.on('data', function (msg) {
     console.log(msg);
 })
-devSvr.stderr.on('data', function (msg) {
-    console.log(msg);
-})
-devSvr.on('exit', function () {
-    console.log('exit devSvr');
-});
-
-// let ele = exec('./node_modules/.bin/electron . --dev', function (error, stdout, stderr) {
-//     if (error) {
-//         console.error('error: ' + error);
-//         return;
-//     }
-//     console.log(stdout);
-// });
-// ele.stdout.on('data', function (msg) {
-//     console.log(msg);
-// })
-// ele.on('exit', function () {
-//     console.log('exit ele');
-// });
-
-process.on('exit', function () {
-    console.log('已退出');
-    // devSvr.kill('SIGHUP');
-    // devSvr._events.exit();
-})
-process.stdin.setEncoding('utf8');
-process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk === 'exit\r\n') {
-        devSvr.exit();
-        process.exit();
-    }
-    else {
-        if (chunk !== null) console.log('命令错误');
-    }
+ele.on('exit', function () {
+    console.log('exit ele');
 });
